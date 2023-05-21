a411e43dfb7e44decc940c7fd356c07015fc1145
Dustin Murry <dmurry29@yahoo.com>
2023-05-21 00:16:25 -0400

    commit

--- therapymanagement/settings.py
+++ therapymanagement/settings.py
@@ -20,6 +20,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+from pyping.core import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
